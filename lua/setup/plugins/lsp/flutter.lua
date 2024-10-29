08667195fb190242281d7935cf12be29c29835d2
peterlouie <peterlouielinatoc@gmail.com>
2024-10-29 03:40:26 +0100

    added indent line

--- lua/setup/plugins/lsp/flutter.lua
+++ lua/setup/plugins/lsp/flutter.lua
@@ -4,7 +4,9 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'stevearc/dressing.nvim', -- optional for vim.ui.select
-    'dart-lang/dart-vim-plugin',
+    -- 'dart-lang/dart-vim-plugin',
   },
   config = true,
+
+  -- vim.keymap.set("n", "<leader>f", "<cmd>FlutterOutlineToggle<CR>")
 }
